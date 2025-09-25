7cbba1d9122cdc9baf4383d533f74d84faa1d31b
Katarina3105 <katarina.mitrovic@valcon.com>
2025-09-25 13:43:10 +0200

    Finalize Catalog Service

---
+++ catalog-service/src/main/java/com/example/catalog_service/Book.java
@@ -0,0 +1,23 @@
+package com.example.catalog_service;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private double price;
+}
+
